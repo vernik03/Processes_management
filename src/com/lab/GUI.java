5415e966086b7e7cfbbb201797ed0b162ad85b7c
vernik03 <vernik03@gmail.com>
2022-09-09 00:54:46 +0300

    delete s.out

--- src/com/lab/GUI.java
+++ src/com/lab/GUI.java
@@ -15,58 +15,7 @@ public class GUI extends JFrame {
 
 
     public GUI() {
-
-
-
-//        JFrame frame = new JFrame("Процеси, потоки");
-//        Box b1=Box.createVerticalBox();    // Создаем горизонтальный контейнер Box
-//        Box b2=Box.createVerticalBox();    // Создаем горизонтальный контейнер Box
-//        Box b3=Box.createVerticalBox();    // Создаем вертикальный контейнер Box
-//        frame.add(b1);  // Добавляем в форму внешний горизонтальный прямоугольник
-//        //b1.add(Box.createHorizontalStrut(20));    // Добавляем вертикальную рамку высотой 200
-//        b1.add(slider);    // Добавить кнопку 1
-//        //b1.add(Box.createHorizontalStrut(20));
-//        // Добавляем в форму внешний горизонтальный прямоугольник
-//        frame.add(b2);
-//        b2.add(spinner1);
-//        //b2.add(Box.createVerticalGlue());    // Добавляем горизонтальный клей
-//        b2.add(spinner2);
-//        //b2.add(Box.createHorizontalStrut(20));// Добавляем вложенный вертикальный контейнер Box
-//        // Добавляем фиксированную область шириной 100 и высотой 20
-//        //b2.add(Box.createRigidArea(new Dimension(100,20)));
-//        // Добавляем в форму внешний горизонтальный прямоугольник
-//        frame.add(b3);
-//        b3.add(button1);
-//        //b3.add(Box.createHorizontalStrut(20));
-//        // Устанавливаем действие закрытия, заголовок, размер, положение и видимость окна
-//        frame.setResizable(false);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setBounds(300, 100, 400, 300);
-//        frame.setVisible(true);
-//
-//
-//        ThreadLeft threadLeft = new ThreadLeft();
-//        ThreadRight threadRight = new ThreadRight();
-//
-//        slider.setPaintTicks(true);
-//        slider.setMajorTickSpacing(10);
-//        slider.setPaintLabels(true);
-//
-//        button1.addActionListener((ActionEvent e) -> {
-//            threadLeft.start();
-//            threadRight.start();
-//        });
-//
-//
-//        spinner1.addChangeListener((ChangeEvent e) -> {
-//            threadLeft.setPriority((int) spinner1.getValue());
-//        });
-//
-//        spinner2.addChangeListener((ChangeEvent e) -> {
-//            threadRight.setPriority((int) spinner2.getValue());
-//        });
-
-        JFrame frame=new JFrame("Пример программы Java");
+        JFrame frame=new JFrame("Процеси, потоки");
         Box b1=Box.createVerticalBox();
         Box b0=Box.createVerticalBox();
         Box b2=Box.createHorizontalBox();
@@ -110,20 +59,16 @@ public class GUI extends JFrame {
 
         spinner1.addChangeListener((ChangeEvent e) -> {
             threadLeft.setPriority((int) spinner1.getValue());
-            System.out.println(threadLeft.getPriority());
         });
 
         spinner2.addChangeListener((ChangeEvent e) -> {
             threadRight.setPriority((int) spinner2.getValue());
-            System.out.println(threadRight.getPriority());
         });
 
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 threadLeft.run_flag = false;
                 threadRight.run_flag = false;
-//                threadLeft.interrupt();
-//                threadRight.interrupt();
             }
         });
 
